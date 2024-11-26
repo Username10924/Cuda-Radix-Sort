b9cb29f53d435bdb81d2523d2042f0c0860ea331
Username10924 <129519733+Username10924@users.noreply.github.com>
2024-11-26 14:43:16 +0300

    Now can sort 1024 elements (1 block)
    Different warps caused the array to be unstable
    So I divided the sort into 32 threads each clock
    cycle

--- main.cu
+++ main.cu
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cuda_runtime.h>
-#include <time.h>
 using namespace std;
+
 
 __global__ void countingSort(int *d_input, int *d_output, int *d_countArray, int *d_positionArray, int power, int size) {
     // number of previous blocks + tidx in current block
@@ -16,20 +16,23 @@ __global__ void countingSort(int *d_input, int *d_output, int *d_countArray, int
 
     __syncthreads();
 
-
-    if (index == 0 + blockIdx.x * blockDim.x) {
+    // only the first thread in the block builds positionArray
+    if (index == 0) {
         for (int i = 1; i < 10; i++)
             d_positionArray[i] = d_countArray[i - 1] + d_positionArray[i - 1];
     }
 
     __syncthreads();
-
     // atomicAdd will return us the value of the position before addition
-    if (index < size) {
-        int digit = (d_input[index] / power) % 10;
-        // increment respective postition in positionArray by 1 to setup position of next element
-        int pos = atomicAdd(&d_positionArray[digit], 1);
-        d_output[pos] = d_input[index];
+    // threads in different warps editing values at the same time cause a problem of unstable sorting, we have to sort warp by warp
+    for(int i = 32; i <= size; i += 32) {
+        if (index < i && index >= i - 32) {
+            int digit = (d_input[index] / power) % 10;
+            // increment respective postition in positionArray by 1 to setup position of next element
+            int pos = atomicAdd(&d_positionArray[digit], 1);
+            d_output[pos] = d_input[index];
+        }
+        __syncthreads();
     }
     
 }
@@ -50,18 +53,21 @@ __global__ void findmax(int *g_idata, int *d_max) {
   }
   
   if (threadIdx.x == 0) {
-    // the max integer is going to be either at index 0 or 1
     *d_max = sdata[0];
   }
 }
 
 int main() {
     srand(time(0));
+    cudaEvent_t start, stop;
+    float milliseconds = 0;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
     // host memory allocations
-    int size = 60;
+    int size = 1024;
     int *input = (int*)malloc(size * sizeof(int));
     int *output = (int*)malloc(size * sizeof(int));
-    for(int i = 0; i < 60; i++) {
+    for(int i = 0; i < 1024; i++) {
         input[i] = rand() % 1000;
     }
     int *max;
@@ -84,6 +90,8 @@ int main() {
     cudaDeviceSynchronize();
     cudaMemcpy(max, d_max, sizeof(int), cudaMemcpyDeviceToHost);
     int power = 1;
+    // start timer
+    cudaEventRecord(start);
     while (*max / power > 0) {
         // reset arrays for next iteration
         cudaMemset(d_countArray, 0, 10 * sizeof(int));
@@ -93,11 +101,15 @@ int main() {
         cudaDeviceSynchronize();
         cudaMemcpy(output, d_output, size * sizeof(int), cudaMemcpyDeviceToHost);
         cudaMemcpy(d_input, output, size * sizeof(int), cudaMemcpyHostToDevice);
-
+        
         // next least significant digit
         power *= 10;
     }
-
+    // end timer
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&milliseconds, start, stop);
+    float seconds = milliseconds / 1000;
     cudaMemcpy(output, d_output, size * sizeof(int), cudaMemcpyDeviceToHost);
 
     cudaFree(d_input);
@@ -109,6 +121,8 @@ int main() {
     for (int i = 0; i < size; i++) {
         cout << output[i] << " ";
     }
+    cout << "\n";
+    cout <<"TIME TAKEN TO SORT " << size << " ELEMENTS: " << seconds << " seconds";
 
     return 0;
 }
